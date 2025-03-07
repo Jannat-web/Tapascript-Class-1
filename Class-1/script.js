acdb7e2496e67fd1ec3388bbeb4f39d0651548f6
Jannat-web <jannatun.nayem.isha@gmail.com>
2025-03-07 19:05:25 +0600

    Delete Class-1 directory

--- Class-1/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, tapaScript!");
-console.log("Welcome to 40 Days of JavaScript!");
-document.write("Check the browser console for a message!");
-
-
-// adding text inside HTML element 
-document.getElementById("test_1").innerText = 'This is text inside HTML element through the ID selector'
